3b7962e3baa7ef781423b5e5bd8b2acffae98dea
ujjwal5pro <35834458+ujjwal5pro@users.noreply.github.com>
2018-02-05 00:43:11 +0530

    Initial Commmit

---
+++ Inventry Management/Inventry Management/Inventry_Management.UWP/SQLite_Uwp.cs
@@ -0,0 +1,27 @@
+﻿
+using System.IO;
+using Windows.Storage;
+using SQLite;
+//using XamarinForms.SQLite.SQLite;
+
+namespace Inventry_Management.UWP
+{
+    public class SQLite_Uwp 
+    {
+
+        public SQLite_Uwp()
+        {
+        }
+
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFilename = "MySQLiteDB.db3";
+            string documentsPath = ApplicationData.Current.LocalFolder.Path;
+            var path = Path.Combine(documentsPath, sqliteFilename);
+            // Create the connection
+            var conn = new SQLiteConnection(path);
+            // Return the database connection
+            return conn;
+        }
+    }
+}
