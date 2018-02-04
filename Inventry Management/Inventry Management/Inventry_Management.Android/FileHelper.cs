285b222886b096eaf8c44e5d2441e033f7264e8c
ujjwal5pro <35834458+ujjwal5pro@users.noreply.github.com>
2018-02-05 00:52:49 +0530

    Initial Commit

---
+++ Inventry Management/Inventry Management/Inventry_Management.Android/FileHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Inventry_Management.Droid;
+using Inventry_Management.Extension;
+using SQLite;
+using Xamarin.Forms;
+
+//using XamarinForms.SQLite.Droid.SQLite;
+//using XamarinForms.SQLite.SQLite;
+
+[assembly: Xamarin.Forms.Dependency(typeof(FileHelper))]
+namespace Inventry_Management.Droid
+{
+    public class FileHelper : IFileHelper
+    {
+        public FileHelper() { }
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFilename = "imDB.db3";
+            string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal); // Documents folder
+            var path = Path.Combine(documentsPath, sqliteFilename);
+            // Create the connection
+            var conn = new SQLiteConnection(path);
+            // Return the database connection
+            return conn;
+        }
+    }
+}
